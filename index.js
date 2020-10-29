e4387cb79317914dd1d98032802944602e164373
gabriella-guimaraes <gabigara13@gmail.com>
2020-10-28 22:33:16 -0300

    Adição de funções e eventos para os botões de cifrar e decifrar.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+let btn = document.getElementById("button");
+function clicar(event){
+}    
+addEventListener('click', clicar)
+let textoParaCifrar = document.getElementById("texto").value
+let offsetKey = document.getElementById("offsetkey").value
+console.log(textoParaCifrar);
+console.log(offsetKey);
+
+//obter o número da posição do array
+//converter para o alfabeto ASC
+
