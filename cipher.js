9bef2f347bb41467c35c26795f13baecee8c5335
gabriella-guimaraes <gabigara13@gmail.com>
2020-11-06 22:14:23 -0300

    melhoria da lógica da cifra. Adição de if e else. Correções em erros

--- cipher.js
+++ cipher.js
@@ -1,27 +1,45 @@
-
-const alphabetSize = 26;
-const cod1aAlphabet = 65;
-
-let finalMsgEncoded = "";
-
-//fórmula: ((códigoDaLetraASC - códigoDaLetra + deslocamento)%26 ==> tamanho do alfabeto) + códigoDaLetra
-//função de critografar
 const cipher = { 
-  encode (offsetKey,msgToCod){
-    msgToCod = '';
-    for (let i=0; i<msgToCod.lenght; i++){
-      let encodedMsg = ((msgToCod.charcodeAt(i) - cod1aAlphabet + offsetKey) % alphabetSize) + cod1aAlphabet;
-      let final = String.fromCharCode(encodedMsg);
-      finalEncodedMsg += final
-      return finalEncodedMsg
-    }
+ 
+    encode: function(userMessage, offset){
+        
+        const codeASC = 65;
+        const alphabetSize = 26;
+        let finalMsgEncoded = "";
+
+        if(offset == "" || offset == null){
+          throw new TypeError ("o usuário deve preencher o campo corretamente")  
+        } else{
+          for (let i=0; i<userMessage.length; i++){
+              if(userMessage.charCodeAt(i) == ""){
+                  finalMsgEncoded +=""
+              } else {
+                 let encodedMsg = ((userMessage.charCodeAt(i) - codeASC) + offset) %alphabetSize + codeASC;
+                 let final = String.fromCharCode(encodedMsg);
+                 finalMsgEncoded += final
+                }
+          } return finalMsgEncoded;
+         }
+    },
     
+    decode: function (userMessage, offset){
+        const codeASC2 = 90;
+        const alphabetSize = 26;
+        let finalMsgDecoded = "";
 
-  }
- };
-  
- 
-
-
- 
+        if(offset == "" || offset == null){
+            throw new TypeError ("o usuário deve preencher o campo corretamente")  
+       } else {
+        for(let i=0; i<userMessage.length; i++){
+          if(userMessage.charCodeAt(i) == ""){
+                finalMsgDecoded +=""
+            } else {
+               let decodedMsg = ((userMessage.charCodeAt(i) - codeASC2) - offset)%alphabetSize + codeASC2;
+               let final2 = String.fromCharCode(decodedMsg);
+               finalMsgDecoded +=final2
+            }
+        } return finalMsgDecoded;
+    }
+    }
+    };
+    
 export default cipher;
